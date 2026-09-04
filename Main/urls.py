663911e0af3b14d7e689bb5d52b50dbfb673b460
parraromerojulian36-ui <parraromerojulian36@gmail.com>
2026-09-03 20:14:21 -0500

    Conexion url Login - Gestionar_Ventas y Formulario para Runserver

--- Main/urls.py
+++ Main/urls.py
@@ -5,11 +5,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('login/', views.home, name='home'),
+    path('', RedirectView.as_view(url='/login/', permanent=False)),
     path('Inventario/', include('Gestion_Inventario.urls')),
     path('ventas/', include('Gestion_Ventas.urls')),
     path('usuarios/', include('Usuarios.urls')),
