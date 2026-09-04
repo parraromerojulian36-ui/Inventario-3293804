a37f2a559e15e89bea685e0e172f72d172727ef0
parraromerojulian36-ui <parraromerojulian36@gmail.com>
2026-09-03 19:56:50 -0500

    Union Login con Formulario con Gestion_Ventas

--- Main/settings.py
+++ Main/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = 'Usuarios'
 
+
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('empresa/', views.datos_empresa, name='datos_empresa'),
 ]
