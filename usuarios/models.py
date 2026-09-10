f7b660e32216411e8b458911be5d069b8a2051e3
dany_castiblanco <danycastiblancot@gmail.com>
2026-09-09 20:41:16 -0500

    Crear modelo Sede

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Sede(models.Model):
+    nombreSede = models.CharField(max_length=100)
+    municipio = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=20)
+    estado = models.BooleanField(default=True)
