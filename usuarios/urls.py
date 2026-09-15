ed2fc6e18d5fc6268292133f403f3b41e9945f34
Jesus_Camacho <jesuscamachopacheco2005@gmail.com>
2026-09-15 08:27:12 -0500

    Actualizado el Usario a usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'Usuarios'
+app_name = 'usuarios'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('empresa/', views.crear_empresa, name='crear_empresa'),
+    path('crear_empresa/', views.crear_empresa, name='crear_empresa'),
     path('empresa/editar/<int:id_empresa>/', views.editar_empresa, name='editar_empresa'),
 ]
 
